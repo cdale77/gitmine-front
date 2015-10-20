31afafb53875af1ab6ad5d0df45b12a5b303582c
Brad Johnson <cdale77@gmail.com>
2015-10-20 16:10:07 -0700

    update firebase url

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import CommitList from "./CommitList";
 
 React.render(
-  <CommitList firebaseUrl="https://go-git.firebaseio.com/.json" />,
+  <CommitList firebaseUrl="https://gitmine.firebaseio.com/.json" />,
   document.getElementById("app")
 );
 
