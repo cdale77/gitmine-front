2022276cb569c1b24586fa09dc1d10fe64c1adaa
Brad Johnson <cdale77@gmail.com>
2015-10-22 12:26:32 -0700

    better styling

--- src/js/CommitListing.js
+++ src/js/CommitListing.js
@@ -4,13 +4,13 @@ class CommitListing extends React.Component {
   render() {
     return (
       <div className="commit-listing">
-        <div className="avatar">
+        <div className="avatar commit-item">
           <a href={"https://github.com/" + this.props.login}>
             <img src={this.props.avatar} />
           </a>
         </div>
-        <div className="date">{this.props.date.split("T")[0]}</div>
-        <div className="message">
+        <div className="date commit-item">{this.props.date.split("T")[0]}</div>
+        <div className="message commit-item">
           <a href={this.props.url}>{this.props.message}</a>
         </div>
       </div>
